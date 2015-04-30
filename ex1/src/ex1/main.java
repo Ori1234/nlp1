b2cb5b6496172b7cbc6823931a2d5c96321becf4
oriterne <oriterner@gmail.com>
2015-04-30 10:45:46 +0300

    try

---
+++ ex1/src/ex1/main.java
@@ -0,0 +1,16 @@
+package ex1;
+
+public class main {
+
+	public static void main(String[] args) {
+		parseARGS(args);
+		//
+		
+	}
+
+	private static void parseARGS(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
